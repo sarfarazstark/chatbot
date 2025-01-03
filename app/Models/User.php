6b9aa840021e0480ec11268c79d5aea243516f0d
sarfarazstark <sarfarazstark@gmail.com>
2025-01-03 20:43:59 +0530

    Add chats relationship to User model and refactor method syntax

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -38,11 +37,14 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function chats() {
+        return $this->hasMany(Chat::class);
     }
 }

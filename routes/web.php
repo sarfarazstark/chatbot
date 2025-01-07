949ac0f31adadde006479b5e294e6b8e6e6c77c8
sarfarazstark <sarfarazstark@gmail.com>
2025-01-07 20:27:19 +0530

    Refactor chat route names for consistency and clarity

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/chat', [MessageController::class, 'index'])->name('chat'); // Show chat interface
-    Route::post('/chat', [MessageController::class, 'store'])->name('chat'); // Send a new message
-    Route::delete('/chat', [MessageController::class, 'destroy'])->name('message.destroy');
+    Route::post('/chat', [MessageController::class, 'store'])->name('chat.store');
+    Route::get('/chat', [MessageController::class, 'index'])->name('chat.index');
+    Route::delete('/chat', [MessageController::class, 'destroy'])->name('chat.destroy');
 });
 
 Route::middleware('auth')->group(function () {
